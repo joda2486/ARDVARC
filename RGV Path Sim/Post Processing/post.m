1f70b754e481f270daaa5650fe9bf19b1b8fe80b
SpleenFiesta <armo7071@colorado.edu>
2024-04-05 13:26:31 -0600

    Update post.m
    basic framework

--- RGV Path Sim/Post Processing/post.m
+++ RGV Path Sim/Post Processing/post.m
@@ -1,3 +1,42 @@
 clc; clear all
 
-bag = rosbag(file)
+
+%% Initialize
+% Read File
+bag = rosbag('filename.bag');
+
+rosbag info 'filename.bag' % display information
+
+% Initilize Variables
+times = select(bag,'TimeStamp');
+% repeat w/ rgv ID, azimuth, elevations
+bluetooth = [times,IDs,azimuths,elevations];
+
+cam = select(bag,'Camera'); % camera files 
+
+pose = select(bag,'Topic','/ardvarc/pose');
+posestruct = readMessages(pose,'DataFormat','Struct');
+
+
+%% Frame Transformations
+% run transformation function on bluetooth data
+
+frames = bag.AvailableFrames; % list of available frame transformations
+tf = getTransform(bag,'world',frames{1}); % latest frame transform
+
+
+%% Computer Vision
+% read image, identify bounding box
+img = rosReadImage(msg);
+
+%% Poses
+
+% plot path
+xPoints = cellfun(@(m) double(m.X),posestruct);
+yPoints = cellfun(@(m) double(m.Y),posestruct);
+zPoints = cellfun(@(m) double(m.Z),posestruct);
+plot(xPoints,yPoints,zPoints)
+
+
+%% Functions
+
